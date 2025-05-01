3e9ba9cec3a7e30d27f7702c323296c0cf74e3b8
Pavithra-py <pavithra210907@gmail.com>
2025-05-01 19:34:56 +0530

    Add files via upload
    Added task1 and task2 python scripts

---
+++ task1.py
@@ -0,0 +1,12 @@
+a=input('Enter the first number: ')
+a=float(a)
+b=input('Enter the second number: ')
+b=float(b)
+c=a+b
+print('Addition: ',c)
+d=a-b
+print('Subtraction: ',d)
+e=a*b
+print('Multiplication:',e)
+f=a/b
+print('Division: ',f)
